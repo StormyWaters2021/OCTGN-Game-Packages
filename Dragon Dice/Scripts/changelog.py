7ef951b99e4158b982dc7c48b5105c0485b6c63e
StormyWaters2021 <143759630+StormyWaters2021@users.noreply.github.com>
2025-08-14 00:44:03 -0700

    Added Dragon Dice

---
+++ Dragon Dice/Scripts/changelog.py
@@ -0,0 +1,26 @@
+changelog = {
+    1020105: ("1.2.1.5", "16 April 2024", [ 
+        "Added a changelog dialog that displays when a game is started.",
+        "Fixed a bug in the roll calculator.",
+        ]),}
+
+
+def show_changelog():
+    mute()
+    #### LOAD UPDATES
+    v1, v2, v3, v4 = gameVersion.split('.')  ## split apart the game's version number
+    v1 = int(v1) * 1000000
+    v2 = int(v2) * 10000
+    v3 = int(v3) * 100
+    v4 = int(v4)
+    button_list = ["Hide until next update", "Show again next launch"]
+    currentVersion = v1 + v2 + v3 + v4  ## An integer interpretation of the version number, for comparisons later
+    lastVersion = getSetting("lastVersion", convertToString(currentVersion - 1))  ## -1 is for players experiencing the system for the first time
+    lastVersion = int(lastVersion)
+    for log in sorted(changelog):  ## Sort the dictionary numerically
+        if lastVersion < log:  ## Trigger a changelog for each update they haven't seen yet.
+            stringVersion, date, text = changelog[log]
+            updates = '\n\n- '.join(text)
+            choice = askChoice("What's new in {} ({}):\n\n-{}".format(stringVersion, date, updates), button_list)
+            if choice == 1:
+                setSetting("lastVersion", convertToString(currentVersion))  ## Store's the current version to a setting
