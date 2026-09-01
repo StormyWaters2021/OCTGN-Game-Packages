f02e18035c9efd5fc48f4ea2129d8006f583248e
StormyWaters2021 <thomas.j.alsteen@gmail.com>
2026-09-01 14:19:48 -0700

    Working on Venom Galactus

--- HeroClix/Scripts/multidial.py
+++ HeroClix/Scripts/multidial.py
@@ -2,28 +2,27 @@ GALACTUS = "1fddb83d-8c87-4403-9ea2-eaa2f9d6a58b"
 GALACTUS_CONVERTER = "ffce23a6-3530-4c1a-89cc-5df238e5d9ae"
 GALACTUS_HERALD = "ef162aa8-ada3-415f-9f2e-062e564adb05"
 
+VENOM_GALACTUS = "fc3fe1ca-34f8-4e92-af49-e14bb0be2ae4"
+VG_TENDRILS = "650df7ac-847b-49f9-a785-81b21ed9f63d"
+VG_BREAKER = "73e8df4a-9d46-4c56-a993-efd0efe5dafe"
+VG_MAW = "1e64fbb4-8e8c-4f27-9a45-fdec59fe1af3"
+VG_HUNGER = "c5ab1d84-9c9b-42d1-9356-57f758af7208"
+
 MULTI_DIAL = {
     GALACTUS: [GALACTUS_CONVERTER, GALACTUS_HERALD],
-}
+    VENOM_GALACTUS: [VG_TENDRILS, VG_BREAKER, VG_MAW, VG_HUNGER],
+            }
 
-MULTI_DIAL_LIST = [GALACTUS, GALACTUS_CONVERTER, GALACTUS_HERALD, ]
+MULTI_DIAL_LIST = [item for key, value in MULTI_DIAL.items() for item in [key] + value]
 
+
+# # GALACTUS # #
 
 def _is_galactus(cards, x=0, y=0):
     mute()
     if cards[0].alternate == "":
         return False
     return cards[0].model == GALACTUS
-
-
-def _find_multidial_base(guid):
-    if guid in MULTI_DIAL:
-        return guid
-
-    for base, values in MULTI_DIAL.items():
-        if guid in values:
-            return base
-    return None
 
 
 def build_galactus(card, x, y):
@@ -39,6 +38,82 @@ def advance_converter(card, x=0, y=0):
 def advance_herald(card, x=0, y=0):
     mute()
     _advance_secondary_dial(card, 1)
+
+
+def reverse_converter(card, x=0, y=0):
+    mute()
+    _reverse_secondary_dial(card, 0)
+    
+    
+def reverse_herald(card, x=0, y=0):
+    mute()
+    _reverse_secondary_dial(card, 1)
+
+
+# # VENOM GALACTUS # # 
+
+def _is_venom_galactus(cards, x=0, y=0):
+    mute()
+    if cards[0].alternate == "":
+        return False
+    return cards[0].model == VENOM_GALACTUS
+    
+
+def build_venom_galactus(card, x, y):
+    mute()
+    _create_multidial(VENOM_GALACTUS, x, y)
+
+
+def advance_tendrils(card, x=0, y=0):
+    mute()
+    _advance_secondary_dial(card, 0)
+
+    
+def advance_breaker(card, x=0, y=0):
+    mute()
+    _advance_secondary_dial(card, 1)
+
+
+def advance_maw(card, x=0, y=0):
+    mute()
+    _advance_secondary_dial(card, 2)
+
+
+def advance_hunger(card, x=0, y=0):
+    mute()
+    _advance_secondary_dial(card, 3)
+
+
+def reverse_tendrils(card, x=0, y=0):
+    mute()
+    _reverse_secondary_dial(card, 0)
+
+    
+def reverse_breaker(card, x=0, y=0):
+    mute()
+    _reverse_secondary_dial(card, 1)
+
+
+def reverse_maw(card, x=0, y=0):
+    mute()
+    _reverse_secondary_dial(card, 2)
+
+
+def reverse_hunger(card, x=0, y=0):
+    mute()
+    _reverse_secondary_dial(card, 3)
+
+
+# # HELPERS # # 
+
+def _find_multidial_base(guid):
+    if guid in MULTI_DIAL:
+        return guid
+
+    for base, values in MULTI_DIAL.items():
+        if guid in values:
+            return base
+    return None
 
 
 def _create_multidial(base, x, y):
@@ -102,3 +177,24 @@ def _advance_secondary_dial(card, dial_list_idx):
     return False
     
 
+def _reverse_secondary_dial(card, dial_list_idx):
+    mute()
+    if card.alternate == "":
+        return
+        
+    guid = card.model
+    
+    if guid not in MULTI_DIAL:
+        return False
+
+    if dial_list_idx < 0 or dial_list_idx >= len(MULTI_DIAL[guid]):
+        return False
+
+    x, y = card.position
+    secondary_guid = MULTI_DIAL[guid][dial_list_idx]
+
+    for item in table:
+        if item.model == secondary_guid and item.position == (x, y):
+            return _retreat_dial(item)
+
+    return False
